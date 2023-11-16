f1b406e4882b6500822588a975740446ab0fd136
HOONIR\benig <chcpla1115@nate.com>
2023-11-16 17:56:57 +0900

    ui ux lovelove

--- app/src/main/java/com/example/getiskin/SkinAnalysisScreen.kt
+++ app/src/main/java/com/example/getiskin/SkinAnalysisScreen.kt
@@ -1,6 +1,8 @@
 package com.example.getiskin
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
@@ -23,9 +25,34 @@ import android.net.Uri
 import android.os.Environment
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextButton
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.maps.model.LatLng
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -48,7 +75,11 @@ fun SkinAnalysisScreen(navController: NavController) {
         if (isGranted) {
             hasCameraPermission = true
         } else {
-            Toast.makeText(context, "Camera permission is required to take photos", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                context,
+                "Camera permission is required to take photos",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
@@ -84,32 +115,210 @@ fun SkinAnalysisScreen(navController: NavController) {
         imageUri = uri
         // TODO: imageUri를 사용하여 화면에 이미지를 표시하거나 다른 처리를 합니다.
     }
-
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(android.graphics.Color.parseColor("#ffffff")))
     ) {
-        Button(onClick = {
-            if (hasCameraPermission) {
-                val uri = createImageUri()
-                cameraLauncher.launch(uri)
-            } else {
-                cameraPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        // 상단 이미지
+        Image(
+            painter = painterResource(id = R.drawable.logo), // 이미지 리소스 ID로 변경
+            contentDescription = null, // contentDescription은 필요에 따라 추가
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp) // 이미지 높이 조절
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+
+            ) {
+            Spacer(modifier = Modifier.height(80.dp))
+            // Text
+            Text(
+                text = "피 부 측 정",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(android.graphics.Color.parseColor("#F7F1E5")))
+                    .padding(20.dp)
+                    .height(30.dp), // 여백 추가
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp, // 원하는 크기로 조절
+                fontWeight = FontWeight.Bold,
+                color = Color(android.graphics.Color.parseColor("#e39368")) // 원하는 색상으로 조절
+            )
+
+            // 중앙 컨텐츠
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+
+
+                // Button
+                Button(
+                    onClick = {
+                        if (hasCameraPermission) {
+                            val uri = createImageUri()
+                            cameraLauncher.launch(uri)
+                        } else {
+                            cameraPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .padding(10.dp)
+                        .border(
+                            1.dp,
+                            Color(android.graphics.Color.parseColor("#e39368")),
+                            shape = RoundedCornerShape(10)
+                        ),
+                    colors = ButtonDefaults.buttonColors(
+                        Color(0xFFE39368), // 버튼 배경색상 설정
+                        contentColor = androidx.compose.ui.graphics.Color.White // 버튼 내부 텍스트 색상 설정
+                    ),
+                    shape = RoundedCornerShape(10)
+                )
+                {
+                    Text(
+                        text = "카메라로 촬영하기",
+                        textAlign = TextAlign.Center,
+                        color = androidx.compose.ui.graphics.Color.White,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = {
+                        galleryLauncher.launch("image/*")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .padding(10.dp)
+                        .border(
+                            1.dp,
+                            Color(android.graphics.Color.parseColor("#e39368")),
+                            shape = RoundedCornerShape(10)
+                        ),
+                    colors = ButtonDefaults.buttonColors(
+                        Color(0xFFE39368), // 버튼 배경색상 설정
+                        contentColor = androidx.compose.ui.graphics.Color.White // 버튼 내부 텍스트 색상 설정
+                    ),
+                    shape = RoundedCornerShape(10)
+                )
+                {
+                    Text(
+                        text = "앨범에서 사진 선택하기",
+                        textAlign = TextAlign.Center,
+                        color = androidx.compose.ui.graphics.Color.White,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                // 선택한 사진의 URI를 화면에 표시합니다. (선택적)
+                imageUri?.let { uri ->
+                    Text(text = "선택된 이미지 URI: $uri")
+                }
+
             }
-        }) {
-            Text(text = "카메라로 촬영하기")
+
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            TextButton(
+                onClick = {
+                    navController.navigate("result")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "제출",
+                    color = Gray,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp)) // 간격 조절
+
+            TextButton(
+                onClick = {
+                    navController.navigate("home")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+
+            ) {
+                Text(
+                    text = "홈으로",
+                    color = Gray,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
 
-        Button(onClick = {
-            // 앨범 런처를 실행합니다.
-            galleryLauncher.launch("image/*")
-        }) {
-            Text(text = "앨범에서 사진 선택하기")
-        }
 
-        // 선택한 사진의 URI를 화면에 표시합니다. (선택적)
-        imageUri?.let { uri ->
-            Text(text = "선택된 이미지 URI: $uri")
-        }
     }
+
 }
+
+
+//Column(
+//modifier = Modifier
+//.fillMaxWidth()
+//.padding(16.dp),
+//verticalArrangement = Arrangement.Bottom
+//) {
+//    Spacer(modifier = Modifier.height(16.dp)) // 간격 조절
+//
+//    TextButton(
+//        onClick = {
+//             "홈으로" 버튼 클릭 시 수행할 작업 추가
+//             예를 들어, 홈 화면으로 이동하는 네비게이션 등
+//        },
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .height(50.dp)
+//            .background(MaterialTheme.colorScheme.primary)
+//    ) {
+//        Text(
+//            text = "홈으로",
+//            color = White,
+//            fontSize = 18.sp,
+//            fontWeight = FontWeight.Bold,
+//            textAlign = TextAlign.Center,
+//            modifier = Modifier.fillMaxSize()
+//        )
+//    }
+//}
+
+@Preview
+@Composable
+fun SkinAnalysisScreenPreview() {
+    // You can create a preview of the HomeScreen here
+    // For example, you can use a NavController with a LocalCompositionLocalProvider
+    // to simulate the navigation.
+    // Note: This is a simplified example; you may need to adjust it based on your actual navigation setup.
+    val navController = rememberNavController()
+    SkinAnalysisScreen(navController = navController)
+}
+
