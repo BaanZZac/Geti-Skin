d9b125df18ef65ac622562c5acec1187a8a19e78
HOONIR\benig <chcpla1115@nate.com>
2023-11-17 16:27:48 +0900

    splash and some Ui changes 2

--- app/src/main/java/com/example/getiskin/ResultsScreen.kt
+++ app/src/main/java/com/example/getiskin/ResultsScreen.kt
@@ -1,16 +1,143 @@
 package com.example.getiskin
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun HomeReturnButton2(modifier: Modifier, navController: NavController) {
+
+    Box(
+        modifier = Modifier
+            .height(150.dp)
+            .padding(5.dp)
+            .clip(RoundedCornerShape(10))
+            .clickable {
+                navController.navigate("home")
+            }
+    ) {
+        // Image 등의 내용을 넣어줌
+        Image(
+            painter = painterResource(id = R.drawable.home),
+            contentDescription = null,
+            contentScale = ContentScale.Crop, // 이미지가 잘릴 수 있도록 설정
+            modifier = Modifier
+                .fillMaxSize() // 이미지를 꽉 채우도록 설정
+                .aspectRatio(1f)
+                .clip(RoundedCornerShape(10))
+        )
+        Text(
+            text = "홈 화면으로",
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.Transparent) // 텍스트 배경을 투명하게 설정
+        )
+    }
+}
 
 @Composable
 fun ResultsScreen(navController: NavController, predict: Int?, predict2: Int?) {
-    Column {
-        Text(text = "$predict")
-        Text(text = "$predict2")
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(android.graphics.Color.parseColor("#ffffff")))
+
+    ) {
+        // 상단 이미지
+        Image(
+            painter = painterResource(id = R.drawable.logo), // 이미지 리소스 ID로 변경
+            contentDescription = null, // contentDescription은 필요에 따라 추가
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp) // 이미지 높이 조절
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(80.dp))
+            // Text
+            Text(
+                text = "측 정 결 과",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(android.graphics.Color.parseColor("#F7F1E5")))
+                    .padding(20.dp)
+                    .height(30.dp), // 여백 추가
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp, // 원하는 크기로 조절
+                fontWeight = FontWeight.Bold,
+                color = Color(android.graphics.Color.parseColor("#e39368")) // 원하는 색상으로 조절
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(android.graphics.Color.parseColor("#F7F1E5"))) // 원하는 배경 색상으로 설정
+                    .padding(16.dp)
+
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState()),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "$predict")
+                    Text(text = "$predict2")
+                    HomeReturnButton2(modifier = Modifier
+                        .weight(1f), navController )
+                    AdPlaces()
+                    AdPlaces()
+                }
+
+            }
+        }
     }
+
+}
+@Preview
+@Composable
+fun ResultScreenPreview() {
+    // You can create a preview of the HomeScreen here
+    // For example, you can use a NavController with a LocalCompositionLocalProvider
+    // to simulate the navigation.
+    // Note: This is a simplified example; you may need to adjust it based on your actual navigation setup.
+    val navController = rememberNavController()
+    ResultsScreen(navController = navController, predict= 42, predict2= 100)
 }
